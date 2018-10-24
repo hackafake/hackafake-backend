1cd40162f25db44a4186c6f174ea3c01d0a34af7
Gianpaolo Macario <gianpaolo.macario@solarma.it>
2018-10-24 20:55:08 +0000

    Fix #43
    Signed-off-by: Gianpaolo Macario <gianpaolo.macario@solarma.it>

--- server/apis.py
+++ server/apis.py
@@ -12,6 +12,7 @@ import random
 import sys
 
 import logging
+import urllib.parse
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -69,6 +70,8 @@ class ChallengeResource(Resource):
 
         logging.debug('ChallengeResource.get(self=%s)', self)
 
+        logging.debug('FakeNews.objects=%s', FakeNews.objects)
+
         fakes = [fake for fake in FakeNews.objects(is_fake=True) ]
         reals = [fake for fake in FakeNews.objects(is_fake=False) ]
 
@@ -96,21 +99,67 @@ class FakeNewsResource(Resource):
     @api.expect(fake_post_model)
     @api.marshal_with(fake_news_model)
     def post(self):
-        log.warning("Before data = request.json")
+        logging.debug("FakeNewsResource.post(self=%s)", self)
         data = request.json
+        logging.debug("FakeNewsResource.post:Got data=", data)
         try:
             url=data['url']
-            log.info("Before FakeNews.objects.get: url=" + url)
+            logging.debug("FakeNewsResource.post: Before FakeNews.objects.get: url=" + url)
+            #
+            # NOTE: Will raise exception if the url is unknown
+            # Maywe we should instead check if the object exists
+            #
             fake = FakeNews.objects.get(url=data['url'])
+            logging.debug("After FakeNews.objects.get: fake=%s", fake)
         except:
-            # FIXME: Double exception may happen here!
+            logging.debug("Not in cache, creating FakeNews(url=%s, counter=%d)", data['url'], 0)
             fake = FakeNews(url=data['url'], counter = 0)
+            logging.debug("FakeNewsResource.post: fake=%s", fake)
+            #
+            # FIXME: Double exception may happen here!
+            #
             fake.title = get_title_from_url(data['url'])
-            r = requests.get('https://api.fakenewsdetector.org/votes?url={}&title='.format(data['url']))
+            # fake.title = "FIXME: This is a Fake title"
+            logging.debug("FakeNewsResource.post: fake.title=%s", fake.title)
+           
+            # r = requests.get('https://api.fakenewsdetector.org/votes?url={}&title='
+            #        .format(urllib.urlencode(data['url'])))
+            logging.debug("FakeNewsResource.post: data=%s", data)
+            # logging.debug("FakeNewsResource.post: data['url']=%s", data['url'])
+            str_data_url = str(data['url'])
+            logging.debug("FakeNewsResource.post: str_data_url=%s", str_data_url)
+            str_data_url_encoded = urllib.parse.quote(str_data_url)
+            # logging.debug("FakeNewsResource.post: str_data_url_encoded=%s", str_data_url_encoded)
+            request_url = 'https://api.fakenewsdetector.org/votes?url={}&title='.format(str_data_url_encoded)
+            logging.debug("FakeNewsResource.post: request_url=%s", request_url)
+            r = requests.get(request_url);
+            logging.debug("FakeNewsResource.post: Got r=%s", r)
+            logging.debug("FakeNewsResource.post: Got r.json()=%s", r.json())
+            #
+            # FIXME: Double exception may happen here!
+            #
+            t_people = r.json()['people']
+            logging.debug("FakeNewsResource.post: Got t_people=%s", t_people)
+            #
+            t_content = t_people['content']
+            logging.debug("FakeNewsResource.post: Got t_content=%s", t_content)
+            #
+            t_robot = r.json()['robot']
+            logging.debug("FakeNewsResource.post: Got t_robot=%s", t_robot)
+            #
+            t_fakenews = r.json()['robot']['fake_news']
+            logging.debug("FakeNewsResource.post: Got t_fakenews=%s", t_fakenews)
+            #
             chance = 0.0
-            for d in r.json()['content']['robot']:
-                if d['category_id'] == 2:
-                    chance = d['chance']
+            # for d in r.json()['people']['content']:
+            #     logging.debug("FakeNewsResource.post: Inside loop: Got d=%s", d)
+            #     if d['category_id'] == 2:
+            #         chance = d['chance']
+            #         logging.debug("FakeNewsResource.post: Inside loop: Got chance=%s", chance)
+            #
+            chance = r.json()['robot']['fake_news'];
+            #
+            logging.debug("FakeNewsResource.post: Overall chance=%s", chance)
             fake.is_fake = chance > 0.5
         fake.counter += 1
         fake.save()
@@ -125,5 +174,28 @@ class FakeNewsResource(Resource):
         user.save()
 
         return fake
+
+# Got r.json()=
+'''
+  'robot': {
+      'clickbait': 0.37361637,
+      'fake_news': 0.0,
+      'extremely_biased': 0.0
+  },
+  'domain': None,
+  'people': {
+      'title': {
+          'clickbait': False,
+          'count': -1
+      },
+      'content': [
+          {'count': 1, 'category_id': 1}
+      ]
+  },
+  'keywords': []
+}
+'''
+
+
 
 # EOF
